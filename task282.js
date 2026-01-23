40eb4984e87fc2f5258d041a68e8a35000294c6a
Purav Patel <patel.purav2024@vitstudent.ac.in>
2026-01-23 14:09:48 +0530

    Create task282.js

---
+++ task282.js
@@ -0,0 +1,7 @@
+function changeText() {
+  // Get the paragraph element by its ID
+  const paragraph = document.getElementById("myParagraph");
+
+  // Change the innerHTML of the paragraph
+  paragraph.innerHTML = "Welcome to JavaScript!";
+}
